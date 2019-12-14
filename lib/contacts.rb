9c021a0c0e218ae6021413e9e9a294391b09a9d9
Blake Marengo <marengo.blake@gmail.com>
2019-12-14 01:47:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,3 +23,15 @@ def remove_strawberry(contacts)
     end
   end
 end
+
+# Alternative solution
+
+def remove_strawberry(contacts)
+  freddy = contacts["Freddy Mercury"
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
+end
